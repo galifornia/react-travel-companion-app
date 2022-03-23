d0dd87a7b363ea0aedc30162318be0f2768bbfac
galifornia <martinho87@gmail.com>
2022-03-23 14:29:55 +0100

    Add components folder with components skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header />
+      <List />
+      <Map />
+      <PlaceDetails />
+    </>
   );
 };
 
