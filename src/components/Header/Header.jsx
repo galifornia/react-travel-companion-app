ae234858eb15b5d1fe29dd875b52bec95036eb36
galifornia <martinho87@gmail.com>
2022-03-23 18:35:23 +0100

    Header_ fix missing renderInput for Autocomplete mui component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,38 @@
-import { AppBar, Box, InputBase, Toolbar, Typography } from '@material-ui/core';
-import { Autocomplete } from '@material-ui/lab';
-import React from 'react';
-import SearchIcon from '@material-ui/icons/Search';
+import { AppBar, Box, InputBase, Toolbar, Typography } from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
+import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
 
-import useStyles from './styles';
+import useStyles from "./styles";
 
 const Header = () => {
   const classes = useStyles();
   return (
-    <AppBar position='static'>
+    <AppBar position="static">
       <Toolbar className={classes.toolbar}>
-        <Typography variant='h5' className={classes.title}>
+        <Typography variant="h5" className={classes.title}>
           Travel Advisor
         </Typography>
-        <Box display='flex'>
-          <Typography variant='h6' className={classes.title}>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          <Autocomplete>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
+          <Autocomplete
+            renderInput={(params) => (
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  placeholder="Search..."
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                />
               </div>
-              <InputBase
-                placeholder='Search...'
-                classes={{ root: classes.inputRoot, input: classes.inputInput }}
-              />
-            </div>
-          </Autocomplete>
+            )}
+          ></Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
