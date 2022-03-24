62e160c92cc2d6df5ba7c609e3f149e05032f3c4
galifornia <martinho87@gmail.com>
2022-03-24 16:08:05 +0100

    Map: flesh out layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,31 @@
-import { AppBar, Box, InputBase, Toolbar, Typography } from "@material-ui/core";
-import { Autocomplete } from "@material-ui/lab";
-import React from "react";
-import SearchIcon from "@material-ui/icons/Search";
+import { AppBar, Box, InputBase, Toolbar, Typography } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
+import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
 
-import useStyles from "./styles";
+import useStyles from './styles';
 
 const Header = () => {
   const classes = useStyles();
   return (
-    <AppBar position="static">
+    <AppBar position='static'>
       <Toolbar className={classes.toolbar}>
-        <Typography variant="h5" className={classes.title}>
+        <Typography variant='h5' className={classes.title}>
           Travel Advisor
         </Typography>
-        <Box display="flex">
-          <Typography variant="h6" className={classes.title}>
+        <Box display='flex'>
+          <Typography variant='h6' className={classes.title}>
             Explore new places
           </Typography>
           <Autocomplete
+            options={[]}
             renderInput={(params) => (
               <div className={classes.search}>
                 <div className={classes.searchIcon}>
                   <SearchIcon />
                 </div>
                 <InputBase
-                  placeholder="Search..."
+                  placeholder='Search...'
                   classes={{
                     root: classes.inputRoot,
                     input: classes.inputInput,
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,29 @@
+import { useMediaQuery } from '@material-ui/core';
+import GoogleMapReact from 'google-map-react';
 import React from 'react';
 
+import useStyles from './styles';
+
 const Map = () => {
-  return <h1>Map</h1>;
+  const classes = useStyles();
+  const coordinates = { lat: 0, lng: 0 };
+
+  const isMobile = useMediaQuery('(min-width: 600px)');
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={[]}
+        onChange={() => console.log('changing')}
+        onChildClick={() => console.log('clicking in child')}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
