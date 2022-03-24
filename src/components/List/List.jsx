1d98253546e0d0bc4fc8bc8f6a895fdd6b9c655d
galifornia <martinho87@gmail.com>
2022-03-24 17:00:10 +0100

    List: complete layout

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import {
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from '@material-ui/core';
+import React, { useState } from 'react';
+import useStyles from './styles';
 
 const List = () => {
-  return <h1>List</h1>;
+  const classes = useStyles();
+  const [filter, setFilter] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+  const places = [
+    { name: 'Chill Place' },
+    { name: 'Cool Place' },
+    { name: 'bad Place' },
+  ];
+
+  return (
+    <div className={classes.container}>
+      <Typography variant='h4'>
+        Restaurants, Hotels & Attractions around you
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={filter} onChange={(ev) => setFilter(ev.target.value)}>
+          <MenuItem value='restaurants'>Restaurants</MenuItem>
+          <MenuItem value='hotels'>Hotels</MenuItem>
+          <MenuItem value='attractions'>Attractions</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        {/* TODO: replace Select with Slider */}
+        <Select value={rating} onChange={(ev) => setRating(ev.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
+        </Select>
+      </FormControl>
+
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => {
+          return (
+            <Grid item key={i} xs={12}>
+              <div>{JSON.place}</div>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </div>
+  );
 };
 
 export default List;
